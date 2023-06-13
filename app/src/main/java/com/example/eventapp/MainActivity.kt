f91fb3e2dffb0e3a290ef917775efa3624878d00
Maciej Topór <maciej.topor@10clouds.com>
2023-06-13 19:52:58 +0200

    Implementing example button and checkbox

--- app/src/main/java/com/example/eventapp/MainActivity.kt
+++ app/src/main/java/com/example/eventapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.eventapp
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.CheckBox
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -28,10 +30,10 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = findViewById(R.id.drawerLayout)
         navigationView = findViewById(R.id.navigationView)
         drawerToggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            R.string.drawer_open,
-            R.string.drawer_close
+                this,
+                drawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close
         )
         drawerLayout.addDrawerListener(drawerToggle)
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
@@ -40,24 +42,27 @@ class MainActivity : AppCompatActivity() {
         // Set listener for drawer menu item selections
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_home -> {
-                    // Handle drawer menu item 3 click
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                R.id.nav_checkbox -> {
+                    val checkBox = menuItem.actionView?.findViewById<CheckBox>(R.id.checkBox)
+                    checkBox?.setOnCheckedChangeListener { _, isChecked ->
+                        if (isChecked) {
+                            // Checkbox is checked
+                        } else {
+                            // Checkbox is unchecked
+                        }
+                    }
                     true
                 }
-                R.id.navigation_dashboard -> {
-                    // Handle drawer menu item 4 click
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+                R.id.nav_button -> {
+                    val button = menuItem.actionView?.findViewById<Button>(R.id.button)
+                    button?.setOnClickListener { l ->
+
+                    }
                     true
                 }
-                R.id.navigation_notifications -> {
-                    // Handle drawer menu item 4 click
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
-                    true
-                }
+
                 else -> false
             }
         }
@@ -67,28 +72,31 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home -> {
                     // Replace the content layout with Fragment1
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.contentLayout, HomeFragment())
-                        .commit()
+                            .replace(R.id.contentLayout, HomeFragment())
+                            .commit()
                     supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
                     true
                 }
+
                 R.id.navigation_dashboard -> {
                     // Replace the content layout with Fragment2
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.contentLayout, DashboardFragment())
-                        .commit()
+                            .replace(R.id.contentLayout, DashboardFragment())
+                            .commit()
                     supportActionBar?.setDisplayHomeAsUpEnabled(true)
                     true
                 }
+
                 R.id.navigation_notifications -> {
                     // Replace the content layout with Fragment2
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.contentLayout, NotificationsFragment())
-                        .commit()
+                            .replace(R.id.contentLayout, NotificationsFragment())
+                            .commit()
                     supportActionBar?.setDisplayHomeAsUpEnabled(false)
                     true
                 }
+
                 else -> false
             }
         }
@@ -96,8 +104,8 @@ class MainActivity : AppCompatActivity() {
         // Set the initial fragment
         navigationView.menu.getItem(0).isChecked = true
         supportFragmentManager.beginTransaction()
-            .replace(R.id.contentLayout, HomeFragment())
-            .commit()
+                .replace(R.id.contentLayout, HomeFragment())
+                .commit()
     }
 
     override fun onPrepareOptionsMenu(menu: Menu): Boolean {
@@ -109,7 +117,7 @@ class MainActivity : AppCompatActivity() {
         this.isDrawerVisible = visible
         invalidateOptionsMenu()
         drawerLayout.setDrawerLockMode(
-            if (visible) DrawerLayout.LOCK_MODE_UNLOCKED else DrawerLayout.LOCK_MODE_LOCKED_CLOSED
+                if (visible) DrawerLayout.LOCK_MODE_UNLOCKED else DrawerLayout.LOCK_MODE_LOCKED_CLOSED
         )
     }
 
